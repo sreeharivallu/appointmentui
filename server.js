53bbb0e0f242e94124601fb82110bba5a519d0de
sreehari vallu <sreehari vallu>
2018-10-29 22:48:16 +0530

    corrected dist path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/appointmentui'));
+app.use(express.static('./dist/doctor-appointment'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/appointmentui/index.html'));
+res.sendFile(path.join(__dirname,'/dist/doctor-appointment/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
